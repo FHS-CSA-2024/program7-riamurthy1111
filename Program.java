f780f1de795e52cc092458b7b66de91ac3c036fb
riamurthy1111 <riamurthy1111@gmail.com>
2024-09-06 09:27:21 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,7 +1,20 @@
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
-
+public class Program7{
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner (System.in);
+        System.out.println("Enter schrute-bucks:");
+        int schruteBucks = 0;
+        schruteBucks = myScanner.nextInt();
+        System.out.println("Enter klevins:");
+        int klevins = 0;
+        klevins = myScanner.nextInt();
+        System.out.println("Enter stanley-nickels:");
+        int stanleyNickels = 0;
+        stanleyNickels = myScanner.nextInt();
+    }
+}
 //Paste console output below:
 /*
 
